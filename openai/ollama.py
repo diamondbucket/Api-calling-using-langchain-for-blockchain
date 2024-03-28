4554cfdc8bc74fd215b5257cc680852d5ac0ee4d
D. Pranav <132156340+diamondbucket@users.noreply.github.com>
2024-03-28 21:58:13 +0530

    Update ollama.py

--- openai/ollama.py
+++ openai/ollama.py
@@ -23,28 +23,3 @@ from langchain.chains.llm import LLMChain
 chain = LLMChain(llm=llm,prompt=prompt)
 topic = input("enter the topic you want some facts about: ")
 print(chain.run({topic}))
-
-'''class Multiply(BaseModel):
-    a: int = Field(...,description="first integer")
-    b: int = Field(...,description="second integer")
-
-openai_api_key = "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"
-llm = ChatOpenAI(model="gpt-3.5-turbo-0125",temperature=0,openai_api_key=openai_api_key)
-llm_with_tools = llm.bind_tools([Multiply])
-#response = llm_with_tools.invoke("what's 3*12")
-
-tool_chain = llm_with_tools | JsonOutputToolsParser()
-response = tool_chain.invoke("what's 3 * 12")
-#from openai import ChatOpenAI, BaseModel, Field  # Assuming imports from OpenAI library
-
-# Assuming BaseModel defines fields
-
-#openai_api_key = "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"
-#llm = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
-
-# Create an instance of Multiply with values
-multiplier = Multiply(a=3, b=12)
-
-llm_with_tools = llm.bind_tools([Multiply])
-response = llm_with_tools.invoke(multiplier)  # Pass the instance to invoke
-print(response)'''
